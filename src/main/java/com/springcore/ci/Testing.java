0a47671057a6f287312f89b2588712d1539fd814
Raushan Tiwari <91769879+raushantiwari024@users.noreply.github.com>
2024-02-06 19:58:04 +0530

    Create Testing.java

---
+++ src/main/java/com/springcore/ci/Testing.java
@@ -0,0 +1,11 @@
+package com.springcore.ci;
+
+public class Testing {
+  public Testing() {
+    System.out.println("Testing constructor invoked");
+  }
+
+  public void getDetails() {
+    System.out.println("Everything going well here!!");
+  }
+}
